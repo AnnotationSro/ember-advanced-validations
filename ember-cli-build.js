9370e4aef9d0346a5dc9ad6ba3bc0bf5edea1535
Igor Kvasnička <IgorKvasn@users.noreply.github.com>
2018-02-14 12:02:34 +0100

    Update ember-cli-build.js
    fix tavis

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -1,7 +1,7 @@
-const EmberAddon = require('ember-cli/lib/broccoli/ember-addon');
+var EmberAddon = require('ember-cli/lib/broccoli/ember-addon');
 
 module.exports = function(defaults) {
-  let app = new EmberAddon(defaults, {
+  var app = new EmberAddon(defaults, {
   });
 
   /*
